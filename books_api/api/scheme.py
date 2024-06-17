dfc5cda67fda7515c7a1814eda2c8382e4c11ded
Priyanka B <priyanka.balakrishna2@gmail.com>
2024-06-17 15:14:42 +0530

    GraphQL mutations

--- books_api/api/scheme.py
+++ books_api/api/scheme.py
@@ -20,3 +20,69 @@ class Query(graphene.ObjectType):
     def resolve_book(self, info, book_id):
         return Book.objects.get(pk=book_id) 
     
+class BookInput(graphene.InputObjectType):
+    id = graphene.ID()
+    title = graphene.String()
+    author = graphene.String()
+    year_published = graphene.String()
+    review = graphene.Int()
+
+
+class CreateBook(graphene.Mutation):
+    class Arguments:
+        book_data = BookInput(required=True)
+    book = graphene.Field(BookType)
+
+    @staticmethod
+    def mutate(root,info,book_data=None):
+        book_instance = Book(
+            title = book_data.title,
+            author = book_data.author,
+            year_published = book_data.year_published,
+            review = book_data.review,
+        )
+        book_instance.save()
+        return CreateBook(book=book_instance)
+    
+
+class UpdateBook(graphene.Mutation):
+    class Arguments:
+        book_data = BookInput(required=True)
+
+    book = graphene.Field(BookType)
+
+    @staticmethod
+    def mutate(root, info, book_data=None):
+
+        book_instance = Book.objects.get(pk=book_data.id)
+
+        if book_instance:
+            book_instance.title = book_data.title
+            book_instance.author = book_data.author
+            book_instance.year_published = book_data.year_published
+            book_instance.review = book_data.review
+            book_instance.save()
+
+            return UpdateBook(book=book_instance)
+        return UpdateBook(book=None)
+    
+class DeleteBook(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID()
+
+    book = graphene.Field(BookType)
+
+    @staticmethod
+    def mutate(root, info, id):
+        book_instance = Book.objects.get(pk=id)
+        book_instance.delete()
+
+        return None
+    
+class Mutation(graphene.ObjectType):
+    create_book = CreateBook.Field()
+    update_book = UpdateBook.Field()
+    delete_book = DeleteBook.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
